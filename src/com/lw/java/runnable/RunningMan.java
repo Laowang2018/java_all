f69ab56dfa249f9a27b35fb0419322d21cc8dbeb
Laowang <wsj6051@126.com>
2020-02-05 11:40:26 +0800

    fix bug

--- src/com/lw/java/runnable/RunningMan.java
+++ src/com/lw/java/runnable/RunningMan.java
@@ -10,10 +10,18 @@ public class RunningMan implements Runnable {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public void run() {
         for(int i=1; i<=100; i++) {
-            System.out.println(this.name + "跑到第" + i +"米了！");
+            System.out.println(this.getName() + "跑到第" + i +"米了！");
         }
     }
 
