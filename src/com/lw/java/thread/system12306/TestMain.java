443733575519d6e3480871fca6a5bff92268bf12
Laowang <wsj6051@126.com>
2020-02-06 18:57:17 +0800

    fixed the common

--- src/com/lw/java/thread/system12306/TestMain.java
+++ src/com/lw/java/thread/system12306/TestMain.java
@@ -6,6 +6,7 @@ public class TestMain {
         Window w1 = new Window("北京北站");
         Window w2 = new Window("北京西站");
         Window w3 = new Window("北京南站");
+
         w1.start();
         w2.start();
         w3.start();
