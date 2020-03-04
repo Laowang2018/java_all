e85d03a7f66097d0555caa05bc4b28332226a3cb
Laowang <wsj6051@126.com>
2020-03-04 22:24:21 +0800

    thread factory new threads

---
+++ src/com/lw/java/thread/factory/MainTest.java
@@ -0,0 +1,15 @@
+package com.lw.java.thread.factory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class MainTest {
+    public static void main(String[] args) {
+//        new Thread(new Printer("good!")).start();
+//        new Thread(new Printer("bad!")).start();
+
+        ThreadFactory factory = Executors.defaultThreadFactory();
+        factory.newThread(new Printer("good!")).start();
+        factory.newThread(new Printer("bad")).start();
+    }
+}
---
+++ src/com/lw/java/thread/factory/Printer.java
@@ -0,0 +1,14 @@
+package com.lw.java.thread.factory;
+
+public class Printer implements Runnable {
+    private String message;
+    public Printer(String message) {
+        this.message = message;
+    }
+    @Override
+    public void run() {
+        for(int i=0; i<1000; i++) {
+            System.out.println(message);
+        }
+    }
+}
