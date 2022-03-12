8435524b87865981cf7eb3415c8555e4ea2b7747
Laowang <wsj6051@126.com>
2022-03-12 21:29:35 +0800

    code upgrade

--- src/com/lw/java/collection/HashSetTest.java
+++ src/com/lw/java/collection/HashSetTest.java
@@ -6,14 +6,19 @@ import java.util.Random;
 public class HashSetTest {
     public static void main(String[] args) {
         Random random = new Random(46);
+
         HashSet<Integer> integers = new HashSet<>();
+
         for(int i=0; i<100; i++) {
             integers.add(random.nextInt(30));
         }
+
         System.out.println(integers.toString());
 
 
         Integer integer = new Integer(10000);
+
         System.out.println(integer.hashCode());
+
     }
 }
